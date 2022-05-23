c27315945e2f1f7a89c6a6639febb0a65a25376f
Juniorhamster <juniorham@yandex.ru>
2022-05-23 15:49:48 +0500

    about page changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,38 +2,38 @@
 
 // Слайдер в блоке меню about-me
 
-$(document).ready(function() {
-   $('.menu-personal-info__title').click(function(event) {
-      $(this).toggleClass('active').nextAll('.menu-personal-info__subtitle').slideToggle(300)
-   })
-})
+// $(document).ready(function() {
+//    $('.menu-personal-info__title').click(function(event) {
+//       $(this).toggleClass('active').nextAll('.menu-personal-info__subtitle').slideToggle(300)
+//    })
+// })
 
-$(document).ready(function() {
-   $('.menu-personal-info__subtitle').click(function(event) {
-      $(this).toggleClass('active').next('.menu-personal-info__text').slideToggle(300)
-   })
-})
+// $(document).ready(function() {
+//    $('.menu-personal-info__subtitle').click(function(event) {
+//       $(this).toggleClass('active').next('.menu-personal-info__text').slideToggle(300)
+//    })
+// })
 
 
-// Добавляет-удаляет белый цвет текста папок
-$(document).ready(function() {
-   $('.menu-personal-info__subtitle').click(function(event) {
-      $(this).toggleClass('white-text')
-   })
-})
+// // Добавляет-удаляет белый цвет текста папок
+// $(document).ready(function() {
+//    $('.menu-personal-info__subtitle').click(function(event) {
+//       $(this).toggleClass('white-text')
+//    })
+// })
 
-// Добавляет белый цвет тексту
-$(document).ready(function() {
-   $('.menu-personal-info__text').click(function(event) {
-      $(this).addClass('white-text')
-   })
-})
+// // Добавляет белый цвет тексту
+// $(document).ready(function() {
+//    $('.menu-personal-info__text').click(function(event) {
+//       $(this).addClass('white-text')
+//    })
+// })
 
-// Удаляет белый цвет у текста
-$(document).ready(function() {
-   $('.menu-personal-info__subtitle').click(function(event) {
-      $('.menu-personal-info__text').removeClass('white-text')
-   })
-})
+// // Удаляет белый цвет у текста
+// $(document).ready(function() {
+//    $('.menu-personal-info__subtitle').click(function(event) {
+//       $('.menu-personal-info__text').removeClass('white-text')
+//    })
+// })
 
 
