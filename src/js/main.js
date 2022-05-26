0e478491d241d5f7babc19cf3c175b1ce03a7802
Juniorhamster <juniorham@yandex.ru>
2022-05-27 02:14:58 +0500

    create slide menu or about me page

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,42 @@
 'use strict'
 
+// ======= Слайдер в блоке меню about-me ОТКРЫТИЕ ЗАКРЫТИЕ блоков START =======//
+
+$(document).ready(function() {
+   $('.personal').click(function() {
+      $(this).addClass('active-icons')
+      $('.professional').removeClass('active-icons')
+      $('.hobbies').removeClass('active-icons')
+      $('.menu-wrapper-professional').slideUp(300)
+      $('.menu-wrapper-hobbies').slideUp(300)
+      $('.menu-wrapper-personal').slideDown(300)
+   })
+})
+
+
+$(document).ready(function() {
+   $('.professional').click(function() {
+      $(this).addClass('active-icons')
+      $('.personal').removeClass('active-icons')
+      $('.hobbies').removeClass('active-icons')
+      $('.bio').removeClass('active-text-color')
+      $('.personal-text-active').slideUp(100)
+      $('.personal-info').slideUp(300)
+      $('.menu-wrapper-personal').slideUp(300)
+      $('.menu-wrapper-hobbies').slideUp(300)
+      $('.menu-wrapper-professional').slideDown(300)
+   })
+})
+
+// ======= Слайдер в блоке меню about-me ОТКРЫТИЕ ЗАКРЫТИЕ блоков END =======//
+
+
+
 // ======= Слайдер в блоке меню about-me personal-info START =======//
 $(document).ready(function() {
    $('.menu-personal-info__title').click(function() {
       $(this).toggleClass('active')
       $('.menu-personal-info__item').slideToggle(300)
-   })
-})
-
-$(document).ready(function() {
-   $('.menu-contacts__title').click(function() {
-      $(this).toggleClass('active')
-      $('.about-menu-contacts').slideToggle(300)
    })
 })
 
@@ -22,7 +47,55 @@ $(document).ready(function() {
       $('.bio').slideToggle(300)
    })
 })
+
+$(document).ready(function() {
+   $('.menu-contacts__title').click(function() {
+      $(this).toggleClass('active')
+      $('.about-menu-contacts').slideToggle(300)
+   })
+})
+
 // ======= Слайдер в блоке меню about-me personal-info END =======//
+
+
+// ======= Слайдер в блоке меню about-me professional-info START =======//
+$(document).ready(function() {
+   $('.menu-professional-info__title').click(function() {
+      $(this).toggleClass('active')
+      $('.menu-professional-info__item').slideToggle(300)
+   })
+})
+
+$(document).ready(function() {
+   $('.info-subtitle-career').click(function() {
+      $(this).toggleClass('active')
+      $(this).toggleClass('white-text')
+      $('.resume').slideToggle(300)
+   })
+})
+
+$(document).ready(function() {
+   $('.info-subtitle-education').click(function() {
+      $(this).toggleClass('active')
+      $(this).toggleClass('white-text')
+      $('.education').slideToggle(300)
+   })
+})
+
+$(document).ready(function() {
+   $('.resume').click(function() {
+      $(this).addClass('active-text-color')
+      $('.education').removeClass('active-text-color')
+   })
+})
+
+$(document).ready(function() {
+   $('.education').click(function() {
+      $(this).addClass('active-text-color')
+      $('.resume').removeClass('active-text-color')
+   })
+})
+// ======= Слайдер в блоке меню about-me professional-info END =======//
 
 
 
@@ -37,42 +110,33 @@ $(document).ready(function() {
 // ======= Слайдер в блоке меню about-me hobbies-info END =======//
 
 
-// ======= Слайдер в блоке меню about-me professional-info START =======//
-$(document).ready(function() {
-   $('.info-subtitle-education').click(function() {
-      $(this).toggleClass('active')
-      $(this).toggleClass('white-text')
-      $('.education').slideToggle(300)
-   })
-})
-// ======= Слайдер в блоке меню about-me professional-info END =======//
-
-
 
 /** ======= Смена цвета подменю при активной ссылке START ==========**/
 $(document).ready(function() {
    $('.bio').click(function() {
       $(this).addClass('active-text-color')
-      $('.interests').removeClass('active-text-color')
-      $('.education').removeClass('active-text-color')
+      $('.personal-text-active').slideDown(300)
+      $('.personal-info').slideDown(300)
+      // $('.interests').removeClass('active-text-color')
+      // $('.education').removeClass('active-text-color')
    })
 })
 
-$(document).ready(function() {
-   $('.education').click(function() {
-      $(this).addClass('active-text-color')
-      $('.bio').removeClass('active-text-color')
-      $('.interests').removeClass('active-text-color')
-   })
-})
+// $(document).ready(function() {
+//    $('.education').click(function() {
+//       $(this).addClass('active-text-color')
+//       $('.bio').removeClass('active-text-color')
+//       $('.interests').removeClass('active-text-color')
+//    })
+// })
 
-$(document).ready(function() {
-   $('.interests').click(function() {
-      $(this).addClass('active-text-color')
-      $('.bio').removeClass('active-text-color')
-      $('.education').removeClass('active-text-color')
-   })
-})
+// $(document).ready(function() {
+//    $('.interests').click(function() {
+//       $(this).addClass('active-text-color')
+//       $('.bio').removeClass('active-text-color')
+//       $('.education').removeClass('active-text-color')
+//    })
+// })
 /** ======= Смена цвета подменю при активной ссылке END ==========**/
 
 
